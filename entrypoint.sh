117d989c3697cd0d62c8b347b21f9149521e93cb
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2023-11-27 00:46:55 +0300

    Fix multiarch build try 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,11 @@ echo "Architecture: $(dpkg --print-architecture)"
 echo "Environment: $(uname -a)"
 
 apt-get update -qq
-apt-get install --no-install-recommends -y -qq git scons build-essential python3 python3-pip flac libspeechd-dev pkg-config
+apt-get install --no-install-recommends -y -qq git scons build-essential python3 python3-pip python-lxml flac libspeechd-dev pkg-config
 python3 -m pip install -U pip
-python3 -m pip install -U setuptools wheel lxml
+python3 -m pip install -U setuptools wheel
 
+git config --global safe.directory *
 python3 setup.py bdist_wheel --dist-dir=github
 
 pip3 install github/*.whl
